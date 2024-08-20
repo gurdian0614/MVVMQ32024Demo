fb1b282dba17f38dd296803a46d4fcd5edae2fcd
gurdian0614 <ivan.gurdian@outlook.com>
2024-08-19 20:33:43 -0600

    Add project files.

---
+++ MVVMQ32024Demo/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MVVMQ32024Demo
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MVVMQ32024Demo/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MVVMQ32024Demo
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
